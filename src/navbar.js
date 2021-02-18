a372326c26d8b6495227bc76a532691a85f7162d
Owusu Bonsu <owusu.bonsu10@gmail.com>
2021-02-18 17:28:03 -0500

    Update navbar css

--- src/navbar.js
+++ src/navbar.js
@@ -1,11 +1,12 @@
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Button } from 'react-bootstrap';
 import React from 'react';
 import './navbar.css'
 
 function NavigationBar() {
         return (
-            <Navbar bg="dark" sticky="top" className="navBack" variant="dark">
-                <Navbar.Brand href="#index" className="nav-text">RateMyStylist</Navbar.Brand>
+            <Navbar bg="light" sticky="top">
+                <Navbar.Brand href="#index" className="nav-tex">RateMyStylist</Navbar.Brand>
+                <Button variant="login" className="loginn">Login</Button>{' '}
             </Navbar>
         )
     }
