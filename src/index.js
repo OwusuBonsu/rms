3749cfde37420c5115c5da28d97cf4bef4a147c6
Owusu Bonsu <owusu.bonsu10@gmail.com>
2021-02-18 17:28:20 -0500

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavigationBar from './navbar';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
