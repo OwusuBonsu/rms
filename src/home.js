c768c534692a213aaa08b713250f982f8361773e
Owusu Bonsu <owusu.bonsu10@gmail.com>
2021-02-19 00:27:08 -0500

    Implement and style carousel

--- src/home.js
+++ src/home.js
@@ -1,21 +1,42 @@
+import {Carousel} from 'react-bootstrap';
+import './home.css'
+import C1 from './C1.jpg'
+import C2 from './C2.jpg'
+import C3 from './C3.jpg'
+import C4 from './C4.jpg'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Carousel controls={false} interval={1000} indicators={false}>
+      <Carousel.Item>
+        <img 
+        className="d-block w-100" 
+        src={C1} 
+        />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img 
+        className="d-block w-100" 
+        src={C2} 
+        />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img 
+        className="d-block w-100" 
+        src={C3} 
+        />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img 
+        className="d-block w-100" 
+        src={C4} 
+        />
+      </Carousel.Item>
+    </Carousel>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavigationBar from './navbar';
+import App from './home'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <NavigationBar />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
